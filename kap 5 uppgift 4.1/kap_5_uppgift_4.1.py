f6d24e7dffe44334fa04bc663bfe87776f7f650a
Admin <Admin@TEB123008>
2022-09-14 10:34:24 +0200

    Add project files.

---
+++ kap 5 uppgift 4.1/kap_5_uppgift_4.1.py
@@ -0,0 +1,5 @@
+a=int(input("Skriv vad (a) ska vara "))
+b=int(input("Skriv vad (b) ska vara "))
+c=int(input("Skriv vad (c) ska vara "))
+if a<b or b<c:
+    print("Antingen ar a mindre an b eller b ar mindre an c")
